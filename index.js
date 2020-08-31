bcbd206b35ef573d177ad7b1105da091ffcbbbb3
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2020-08-31 03:58:12 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,117 @@
+const Express		= require("express");
+const BodyParser	= require("body-parser");
+const FS		= require("fs");
+const HTTPS		= require("https");
+const Path		= require("path");
+
+//Get the Medooze Media Server interface
+const MediaServer = require("medooze-media-server");
+
+//Get Semantic SDP objects
+const SemanticSDP	= require("semantic-sdp");
+const SDPInfo		= SemanticSDP.SDPInfo;
+const MediaInfo		= SemanticSDP.MediaInfo;
+const CandidateInfo	= SemanticSDP.CandidateInfo;
+const DTLSInfo		= SemanticSDP.DTLSInfo;
+const ICEInfo		= SemanticSDP.ICEInfo;
+const StreamInfo	= SemanticSDP.StreamInfo;
+const TrackInfo		= SemanticSDP.TrackInfo;
+const Direction		= SemanticSDP.Direction;
+const CodecInfo		= SemanticSDP.CodecInfo;
+
+const Capabilities = {
+	audio : {
+		codecs		: ["opus"],
+		extensions	: [ "urn:ietf:params:rtp-hdrext:ssrc-audio-level"]
+	},
+	video : {
+		codecs		: ["h264;packetization-mode=1","vp8","vp9","av1"],
+		rtx		: true,
+		rtcpfbs		: [
+			{ "id": "transport-cc"},
+			{ "id": "ccm", "params": ["fir"]},
+			{ "id": "nack"},
+			{ "id": "nack", "params": ["pli"]}
+		],
+		extensions	: [ "http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01"]
+	}
+};
+
+//Get the Medooze Media Server interface
+const MediaServer = require("medooze-media-server");
+
+//Check 
+if (process.argv.length!=3)
+	 throw new Error("Missing IP address\nUsage: node index.js <ip>");
+//Get ip
+const ip = process.argv[2];
+
+//Create UDP server endpoint
+const endpoint = MediaServer.createEndpoint(ip);
+
+const options = {
+	key: fs.readFileSync ("server.key"),
+	cert: fs.readFileSync ("server.cert")
+};
+
+//Enable debug
+MediaServer.enableDebug(false);
+MediaServer.enableUltraDebug(false);
+
+//Restrict port range
+MediaServer.setPortRange(10000,20000);
+
+//Create rest api
+const rest = Express();
+rest.use(BodyParser.json());
+rest.post("/whip/:streamId" , (req, res)=>{
+	//Get streamId
+	const streamId = req.streamId;
+	//Get offer
+	const offer =  SDPInfo.process(req.body);
+	//Get now
+	const ts = Date.now();
+	//Get dump file
+	const file = "/tmp/" + streamId + "-" + ts +".pcap";
+	//Log
+	console.log(streamId + " " +file);
+	//Create an DTLS ICE transport in that enpoint
+	const transport = endpoint.createTransport(offer);
+	
+	transport.on("icetimeout" ,()=>{
+		console.log(streamId +"::icetimeout");
+		transport.stop();
+	});
+	transport.on("dtlsstate" ,(state)=>{
+		console.log(streamId +"::dtlsstate::"+state);
+		if (state=="failed")
+			transport.stop();
+	});
+			
+	//Dump contents
+	transport.dump(file);
+			
+	//Set RTP remote properties
+	transport.setRemoteProperties(offer);
+			
+	//Create local SDP info
+	const answer = offer.answer({
+		dtls		: transport.getLocalDTLSInfo(),
+		ice		: transport.getLocalICEInfo(),
+		candidates	: endpoint.getLocalCandidates(),
+		capabilities	: Capabilities
+	});
+
+	//Set RTP local  properties
+	transport.setLocalProperties(answer);
+
+	//Done
+	res.type("application/sdp");
+	res.send(answer.toString());
+});
+
+
+
+//Create HTTP server
+const server = HTTPS.createServer (options, rest).listen(8080);
+
